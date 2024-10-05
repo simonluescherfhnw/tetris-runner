ad5ac60c369790df26bb357d28345ff7698c8ae2
simon_luescher <simon.luescher@easydoo.ch>
2024-10-05 17:45:27 +0200

    Last changes for build

--- Assets/Scripts/Coin.cs
+++ Assets/Scripts/Coin.cs
@@ -1,7 +1,4 @@
-using JetBrains.Rider.Unity.Editor;
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using System.ComponentModel;
 using UnityEngine;
 
--- Assets/Scripts/UI/MainMenuUI.cs
+++ Assets/Scripts/UI/MainMenuUI.cs
@@ -7,6 +7,7 @@ using UnityEngine.UIElements;
 public class MainMenuUI : MonoBehaviour
 {
     private const string StartButtonName = "StartButton";
+    private const string QuitButtonName = "QuitButton";
     private const string LevelDropDownName = "LevelDropDown";
 
     private UIDocument _mainMenuDocument;
@@ -25,6 +26,15 @@ public class MainMenuUI : MonoBehaviour
             Debug.Log("Start Button Pressed");
             int sceneNr = _mainMenuDocument.rootVisualElement.Q<DropdownField>(LevelDropDownName).index + 1;
             UnityEngine.SceneManagement.SceneManager.LoadScene(sceneNr);
+        };
+        _mainMenuDocument.rootVisualElement.Q<Button>(QuitButtonName).clicked += () =>
+        {
+            Debug.Log("Quit Button Pressed");
+#if !UNITY_EDITOR
+            Application.Quit();
+#elif UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+#endif
         };
     }
 }
